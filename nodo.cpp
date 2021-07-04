8c57ea1e7d1fe4445e70614e8d5e07415edc2016
Macuh <fera1540@gmail.com>
2021-07-04 14:22:18 +0200

    Added fields, getter and setter to Nodo class

--- nodo.cpp
+++ nodo.cpp
@@ -1,3 +1,70 @@
 class Nodo{
+    private:
+        int valueMin;
+        int valueMax;
+
+        Nodo* right;
+        Nodo* left;
+        Nodo* parent;
+
+        int color;
+
+        bool nilNode;
     
+    public:
+        //Getter
+
+        int getValueMin(){
+            return valueMin;
+        }
+
+        int getValueMax(){
+            return valueMax;
+        }
+
+        Nodo* getRight(){
+            return right;
+        }
+
+        Nodo* getLeft(){
+            return left;
+        }
+
+        Nodo* getParent(){
+            return parent;
+        }
+
+        int getColor(){
+            return color;
+        }
+
+        bool checkNil(){
+            return nilNode;
+        }
+
+        //Setter
+
+        void setValueMin(int newValue){
+            valueMin = newValue;
+        }
+
+        void setValueMax(int newValue){
+            valueMax = newValue;
+        }
+
+        void setRight(Nodo* newRight){
+            right = newRight;
+        }
+
+        void setLeft(Nodo* newLeft){
+            left = newLeft;
+        }
+
+        void setParent(Nodo* newParent){
+            parent = newParent;
+        }
+
+        void setColor(int newColor){
+            color = newColor;
+        }
 };
