5ab1f978dafc5e7df9506ae58984966c2053f083
Andrea Cascino <fera1540@gmail.com>
2021-08-04 16:51:07 +0200

    Added comments to IntervalTree class

--- IntervalTree.h
+++ IntervalTree.h
@@ -5,9 +5,33 @@ class IntervalTree : public RedBlackTree{
         IntervalTree() : RedBlackTree(){
 
         }
-        
+        /**
+         * @brief Search a interval that overlap the param value
+         * 
+         * @param value 
+         * @return Nodo* if exists a interval that overlap value, otherwise return a nilNode
+         */
         Nodo* ricerca(int value);
+
+        /**
+         * @brief Call the RedBlackTree::insert method but, after that, update the maxSubtree values of the nodes
+         * 
+         * @param newNodo 
+         */
         void insert(Nodo* newNodo);
+
+        /**
+         * @brief Call the RedBlackTree::elimina method but, after that, update the maxSubtree valures of the nodes
+         * 
+         * @param nodo 
+         */
         void elimina(Nodo* nodo);
+
+        /**
+         * @brief Update maxSubtree values of the nodes with a postOrder tree visit
+         * 
+         * @param newNodo 
+         * @return the max interval up bound contained in the tree
+         */
         int updateMaxSubtree(Nodo* newNodo);
 };
