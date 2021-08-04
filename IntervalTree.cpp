ab7a5bf8f37740daff26fbabd341acb782fac061
Andrea Cascino <fera1540@gmail.com>
2021-08-04 16:27:57 +0200

    Implemented updateMaxSubtree Method

--- IntervalTree.cpp
+++ IntervalTree.cpp
@@ -14,7 +14,23 @@ Nodo* IntervalTree::ricerca(int value){
 }
 
 int IntervalTree::updateMaxSubtree(Nodo* nodoAttuale){
+    int maxLeft = 0;
+    int maxRight = 0;
 
+    if(!nodoAttuale->getLeft()->checkNil())
+        maxLeft = updateMaxSubtree(nodoAttuale->getLeft());
+    
+    if(!nodoAttuale->getRight()->checkNil())
+        maxRight = updateMaxSubtree(nodoAttuale->getRight());
+
+    if(nodoAttuale->getValueMax() < maxLeft || nodoAttuale->getValueMax() < maxRight){
+        if(maxLeft > maxRight)
+            nodoAttuale->setMaxValueInSub(maxLeft);
+        else   
+            nodoAttuale->setMaxValueInSub(maxRight);
+    }
+
+    return nodoAttuale->getMaxValueInSub();
 }
 
 void IntervalTree::intervalInsert(Nodo* newNodo){
